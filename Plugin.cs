8c931b3bfe7598da712cfa35b2f5f64931821e91
Tyre <122072344+Razer109@users.noreply.github.com>
2024-02-11 13:24:59 +1100

    Add files via upload

---
+++ Plugin.cs
@@ -0,0 +1,30 @@
+﻿using BepInEx;
+using System;
+using UnityEngine;
+using Photon.Pun;
+
+namespace TyresStreamingEssentials
+{
+    [BepInPlugin(PluginInfo.GUID, PluginInfo.Name, PluginInfo.Version)]
+    public class Plugin : BaseUnityPlugin
+    {
+        public void OnGUI()
+        {
+            // if(GUI.Button(new Rect(50f, 20f, 400f, 400f), "join random test")) { PhotonNetwork.JoinRandomOrCreateRoom(); }       
+            GUI.skin.box.fontSize = 35;
+            GUI.skin.label.fontSize = 35;
+           
+            
+            // pain
+            GUI.Box(new Rect(1000f, -1f, 400, 400), "Stream Essentials");
+            if (PhotonNetwork.InRoom)
+            {
+                GUI.Label(new Rect(1000f, 40f, 400, 400), PhotonNetwork.CurrentRoom.Name + "\n" + PhotonNetwork.CurrentRoom.PlayerCount + "/" + PhotonNetwork.CurrentRoom.MaxPlayers);
+            }
+            else
+            {
+                GUI.Label(new Rect(1000f, 40f, 400, 400), "Not In Room Right Now!");
+            }
+        }
+    }
+}
---
+++ PluginInfo.cs
@@ -0,0 +1,13 @@
+﻿
+namespace TyresStreamingEssentials
+{
+    /// <summary>
+    /// This class is used to provide information about your mod to BepInEx.
+    /// </summary>
+    class PluginInfo
+    {
+        public const string GUID = "com.tyre.gorillatag.tyresstreamingessentials";
+        public const string Name = "TyresStreamingEssentials";
+        public const string Version = "1.0.0";
+    }
+}
